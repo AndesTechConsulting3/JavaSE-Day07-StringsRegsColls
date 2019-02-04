357fa9a330408d841a306397072045f9bbaeeb0a
a0z9 <a0z9@rambler.ru>
2019-02-04 20:37:07 +0300

    5

--- src/main/java/org/andestech/learning/rfb19/g3/AppCollections.java
+++ src/main/java/org/andestech/learning/rfb19/g3/AppCollections.java
@@ -1,6 +1,7 @@
 package org.andestech.learning.rfb19.g3;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AppCollections {
 
@@ -8,12 +9,23 @@ public class AppCollections {
 
     {
         ArrayList<Double> arrayList =
-                new ArrayList<>(10);
+                new ArrayList<>(4);
 
         arrayList.add(987.123);
         arrayList.add(6676.223);
         arrayList.add(12312.222222);
-        arrayList.add(12312.0);
+        arrayList.add(12.0);
+        arrayList.add(312.0);
+        arrayList.add(12.0);
+
+        arrayList.remove(new Double(12));
+
+        arrayList.add(1, 77.00);
+        arrayList.set(1,88.0);
+
+
+
+        System.out.println(arrayList.size());
 
         ArrayList<Object> arrayList2 =
                 new ArrayList<>(10);
@@ -27,7 +39,19 @@ public class AppCollections {
         arrayList2.add(12312.0);
 
 
+
         System.out.println(arrayList);
+
+        Double[] datas = new Double[arrayList.size()];
+        arrayList.toArray(datas);
+
+        System.out.println(Arrays.toString(datas));
+
+
+        ArrayList<Double> arrayList4 =
+                new ArrayList<>(arrayList);
+        arrayList4.add(88888.888);
+        System.out.println(arrayList4);
 
 
 
